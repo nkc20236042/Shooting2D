d01b9b0ff8e683f251d0b2f9497931dd700f3acd
nkc20236042 <nkc23.koroba.ryo@gmail.com>
2023-06-07 16:19:02 +0900

    6/7

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 5;
+    Vector3 dir = Vector3.zero; //移動方向を保存する変数
+
+    Animator anim;
+
+    void Start()
+    {
+        //アニメーターコンポーネントの情報を保存
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //移動方向をリセット
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += dir.normalized * speed * Time.deltaTime;
+
+        //画面内移動制限
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -9f, 9f);
+        pos.y = Mathf.Clamp(pos.y, -5f, 5f);
+        transform.position = pos;
+
+        //アニメーション設定
+        if (dir.y == 0)
+        {
+            //アニメーションクリップ[Player]を再生
+            anim.Play("Player");
+        }
+        else if (dir.y == 1)
+        {
+            anim.Play("PlayerL");
+        }
+        else if (dir.y == -1)
+        {
+            anim.Play("PlayerR");
+        }
+    }
+}
